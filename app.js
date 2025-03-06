d2ba0100fd0fe79b118b6e617043996bf97d0b50
375211 <124411649+375211@users.noreply.github.com>
2025-03-06 14:50:15 +0800

    zyh

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var cors = require('cors');
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
