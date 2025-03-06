186009edceba21a4a98952b7f0684864bfb60dc5
zcy <2956239093@qq.com>
2025-03-06 11:46:51 +0800

    day3.6

---
+++ module/db.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://2956239093:zcy.521521@cluster0.a6g8b.mongodb.net/myfirstdata");
+
+let LY = new mongoose.Schema({
+    key:String,
+    label:String,
+    p_id:{
+        type:mongoose.Types.ObjectId,
+        ref:'LY'
+    },
+    level:Number
+});
+let LYModel = mongoose.model('LY', LY);
+module.exports = {
+    LYModel
+};
+
