3acc7d9eba651f046adc1b69d2b736e8f1f7c465
375211 <124411649+375211@users.noreply.github.com>
2025-03-11 16:58:24 +0800

    拉取修改

--- module/db.js
+++ module/db.js
@@ -1,37 +1,6 @@
 let mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://2956239093:zcy.521521@cluster0.a6g8b.mongodb.net/myfirstdata");
 
-let LY = new mongoose.Schema({
-    key:String,
-    label:String,
-    p_id:{
-        type:mongoose.Types.ObjectId,
-        ref:'LY'
-    },
-    level:Number
-});
-let Login =new mongoose.Schema({
-    user:String,
-    pwd:String,
-    email:{
-        type:String,
-        default:''
-    },
-    phone:{
-        type:String,
-        default:''
-    },
-    img:{
-        type:String,
-        default:"http://localhost:3100/images/header.jpg"
-    }
-})
-let LYModel = mongoose.model('LY', LY);
-let LoginModel = mongoose.model('Login', Login);
-module.exports = {
-    LYModel,
-    LoginModel
-};
 // let LY = new mongoose.Schema({
 //     key:String,
 //     label:String,
